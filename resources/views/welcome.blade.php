79cffd1643248b7bbbd289a1a00cfc6b905a7dbe
Sazzadhosen10 <hosen15-5836@diu.edu.bd>
2025-06-19 14:37:01 +0600

    xyz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portfolio</title>
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 <body>
-    <h1> </h1>
+    <nav>
+        <div>
+            <h3> Sazzad Hosen</h3>
+        </div>
+        <div>
+             <a href="#">"HOME"</a>
+             <a href="#">About</a>
+             <a href="#">Projects</a>
+             <a href="#">Skills</a>
+        </div>
+
+    </nav>
+
 </body>
 </html>
